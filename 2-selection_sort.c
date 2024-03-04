3ea5e0c93db4ba069687966815546d41e30829cc
Salq24 <qogheneochuko@gmail.com>
2024-03-04 12:42:14 -0800

    select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,9 +8,9 @@
 
 void swap_elem(int *elem1, int *elem2)
 {
-        int other = *elem1;
-        *elem1 = *elem2;
-        *elem2 = other;
+	int other = *elem1;
+	*elem1 = *elem2;
+	*elem2 = other;
 }
 
 /**
@@ -31,11 +31,13 @@ void selection_sort(int *array, size_t size)
 	{
 		smallest = array + x;
 		for (y = x + 1; y < size; y++)
+		{
 		       smallest = (array[y] < *smallest) ? (array + y) : smallest;
+		}
 		if ((array + x) != smallest)
 		{
 		swap_elem(array + x, smallest);
 		print_array(array, size);
 		}
 	}
-}	
+}
